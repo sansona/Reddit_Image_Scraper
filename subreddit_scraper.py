25d0eb803459858dd3db8c11d8316273931244a2
sansona <jiaming.justin.chen@gmail.com>
2019-02-25 19:34:57 -0800

    Working on video functionality

--- subreddit_scraper.py
+++ subreddit_scraper.py
@@ -38,10 +38,11 @@ def WriteIm(im_url, fname):
             with open(fname, 'wb') as f:
                 for chunk in response.iter_content(4096):
                     f.write(chunk)
-        except FileNotFoundError:
+        except (FileNotFoundError, OSError):
             '''
             if submission post has some oddity that makes it incompatible
-            with file naming conventions, rename to some random integer
+            with file naming conventions (backslashes or too long
+            , rename to some random integer
             '''
             new_fname = GenerateRandomFileName()
             with open(sys.argv[1]+new_fname, 'wb') as f:
@@ -84,29 +85,41 @@ def SaveImFromLink(submission_link):
 
 
 def ConvertImToVid(directory):
+    '''
+    (TODO): implement better way of aspect ratio resizing
+    uses cv2.VideoWriter to make video from images scraped. 
+
+    Called w/ -v arg
+    '''
     ims = [file for file in os.listdir(directory) if not file.endswith('.zip')]
     frame = cv2.imread(os.path.join(directory, ims[0]))
     cv2.imshow('video', frame)
-    w, h, channels = frame.shape
+    #w, h, channels = frame.shape
 
+    # get largest (w, h) in images, later resize all to that
+    w_max, h_max = (0, 0)
+    for image in ims:
+        im_path = os.path.join(directory, image)
+        dim_frame = cv2.imread(im_path)
+        w, h, channels = dim_frame.shape
+        if (w > w_max) and (h > h_max):
+            w_max, h_max = w, h
+
+    # cv2 only supports .avi & MJPQ combo, keep as is
     vid_name = directory + '.avi'
-    # something in here isn't working
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    video = cv2.VideoWriter(vid_name, fourcc, 20.0, (w, h))
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    video = cv2.VideoWriter(vid_name, fourcc, 1.0, (w_max, h_max))
 
+    # loop to resize im to (w, h)_max, append to video
     for image in ims:
         im_path = os.path.join(directory, image)
         frame = cv2.imread(im_path)
-
+        frame = cv2.resize(frame, (w_max, h_max))
         video.write(frame)
-
-        cv2.imshow('video', frame)
-        if (cv2.waitKey(1) & 0xFF) == ord('q'):
-            break
 
     video.release()
     cv2.destroyAllWindows()
-    #shutil.move(vid_name, directory+'/'+vid_name)
+    shutil.move(vid_name, directory+'/'+vid_name)
 
 # ------------------------------------------------------------------------------
 
@@ -115,7 +128,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
         description='Scrapes images from subreddit')
-    parser.add_argument('S', help='subreddit to scrape', type=str)
+    parser.add_argument('subreddit', help='subreddit to scrape', type=str)
     parser.add_argument('L', help='max number submissions to scrape',
                         nargs='?', default=25, const=25, type=int)
     parser.add_argument('-v', '--video',
@@ -129,40 +142,40 @@ if __name__ == '__main__':
                           user_agent='',
                           username='',
                           password='')
-    sub = scraper.subreddit(args.S)
+    sub = scraper.subreddit(args.subreddit)
 
     # if first time running, scrape top posts
-    if not os.path.exists(args.S):
+    if not os.path.exists(args.subreddit):
         os.makedirs(sys.argv[1])
         for submission in sub.top(time_filter='all', limit=args.L):
             link = submission.url
             SaveImFromLink(link)
         print('%s images scraped from %s' % ((
-            len(os.listdir(args.S))), args.S))
+            len(os.listdir(args.subreddit))), args.subreddit))
     # if directory exists already, scrape hot posts
     else:
         # archive old files
-        if len(os.listdir(args.S)) > 25:
+        if len(os.listdir(args.subreddit)) > 25:
             date = time.strftime('%Y%m%d')
-            shutil.make_archive(date, 'zip', args.S)
+            shutil.make_archive(date, 'zip', args.subreddit)
 
-            for root, dirs, files in os.walk(args.S):
+            for root, dirs, files in os.walk(args.subreddit):
                 for f in files:
                     if not f.endswith('.zip'):
                         os.remove(os.path.join(root, f))
 
             print('Archived to %s' % date+'.zip')
-            shutil.move(date + '.zip', args.S+'/'+date+'.zip')
+            shutil.move(date + '.zip', args.subreddit+'/'+date+'.zip')
 
         # scrape hot posts if directory already present
-        starting_count = len(os.listdir(args.S))
+        starting_count = len(os.listdir(args.subreddit))
         for submission in sub.hot(limit=args.L):
             link = submission.url
             SaveImFromLink(link)
         print('%s images scraped from %s' % ((
-            len(os.listdir(args.S)) - starting_count), args.S))
+            len(os.listdir(args.subreddit)) - starting_count), args.subreddit))
 
     if args.video:
-        ConvertImToVid(args.S)
+        ConvertImToVid(args.subreddit)
 
         # ------------------------------------------------------------------------------
